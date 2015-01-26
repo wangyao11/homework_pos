f38e556743bfc8726a6870c0f00e09afad269cb4
ywang <657316992@qq.com>
2015-01-26 22:09:15 +0800

    add scanner-test.js and add .scan() test

---
+++ __tests__/scanner-test.js
@@ -0,0 +1,27 @@
+jest.dontMock('../js/model/cart-item');
+jest.dontMock('../js/model/item');
+jest.dontMock('../js/model/scanner');
+
+
+describe('Scanner', function() {
+
+  describe('.scan()', function() {
+
+    it('should return CartItem object', function() {
+
+      var CartItem = require('../js/model/cart-item');
+      var Item = require('../js/model/item');
+      var Scanner = require('../js/model/scanner');
+
+      var itemTest = new Item('ITEM000000', '可口可乐350ml', '瓶', 3.00, '可口可乐');
+      var input = { 'ITEM000000' : 20 };
+
+      var result = Scanner.scan(input);
+
+      var expectResult = new CartItem(itemTest, 20);
+
+      expect(expectResult).toEqual(result);
+
+    });
+  });
+});
