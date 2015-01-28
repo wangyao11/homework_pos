e8e0780c346c65a31384f1a61d9bb2736d3e8deb
ywang <657316992@qq.com>
2015-01-28 20:42:13 +0800

    realize scanner.scan() and pass scanner-test.js scan() test

--- js/model/scanner.js
+++ js/model/scanner.js
@@ -1,10 +1,21 @@
+var _ = require('lodash');
+var CartItem = require('./cart-item');
 function Scanner() {
 
 }
 
-Scanner.scan = function(input) {
+Scanner.scan = function(input, allItems) {
 
-  return '';
+  var barcode = '';
+  var amount = 1;
+
+  for (var key in input) {
+    barcode = key;
+    amount = input[key];
+  }
+  var item = _.find(allItems, {'barcode' : barcode});
+
+  return new CartItem(item, amount);
 
 }
 
