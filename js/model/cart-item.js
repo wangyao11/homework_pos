cf9280c3aa81c8e7e32a0364e040509dbb831be7
ywang <657316992@qq.com>
2015-01-29 20:56:55 +0800

    add cartItemDiscountText() to cartItem class and relize it

--- js/model/cart-item.js
+++ js/model/cart-item.js
@@ -6,7 +6,27 @@ function CartItem(item, amount) {
 }
 
 CartItem.prototype.calculateSubtotal = function () {
-  return this.item.price * this.amount;
+  return this.item.getPrice() * this.amount;
+};
+
+CartItem.prototype.is = function () {
+  return 0;
+};
+
+CartItem.prototype.promotionMoney = function () {
+  return this.item.getPromotionMoney() * this.amount;
+};
+
+CartItem.prototype.promotionText = function () {
+  return '';
+};
+
+CartItem.prototype.cartItemText = function () {
+  return this.item.printText(this.calculateSubtotal(), this.amount);
+};
+
+CartItem.prototype.cartItemDiscountText= function () {
+  return this.item.itemDiscountText(this.promotionMoney());
 };
 
 module.exports = CartItem;
