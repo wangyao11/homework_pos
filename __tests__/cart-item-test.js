b261cfdc1acfb1bd847fabe80f4565e2eb9e9cce
ywang <657316992@qq.com>
2015-01-29 20:55:53 +0800

    add cartItemDiscountText() to cart-item.js

--- __tests__/cart-item-test.js
+++ __tests__/cart-item-test.js
@@ -12,14 +12,34 @@ describe('CartItem', function() {
       var Item = require('../js/model/item');
 
       var itemTest = new Item('ITEM000000', '可口可乐350ml', '瓶', 3.00, '可口可乐');
-      var CartItemTest = new CartItem(itemTest, 20);
+      var cartItemTest = new CartItem(itemTest, 20);
 
       var expectResult = 60;
 
-      var result = CartItemTest.calculateSubtotal();
+      var result = cartItemTest.calculateSubtotal();
 
       expect(expectResult).toBe(result);
-      
+
+    });
+
+  });
+
+  describe('#cartItemDiscountText()', function() {
+
+    it('should return correct null string', function() {
+
+      var CartItem = require('../js/model/cart-item');
+      var Item = require('../js/model/item');
+
+      var itemTest = new Item('ITEM000000', '可口可乐350ml', '瓶', 3.00, '可口可乐');
+      var cartItemTest = new CartItem(itemTest, 20);
+
+      var expectResult = '';
+
+      var result = cartItemTest.cartItemDiscountText();
+
+      expect(expectResult).toBe(result);
+
     });
   });
 });
